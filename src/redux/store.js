3b6e550ceaef7ba21101354b05d4a3ae80484317
SteamDwarf <schajckodmitrij@gmail.com>
2022-07-02 14:42:03 +0300

    replace createStore on configureStore (non-serialized error with persist)

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,7 @@ import { rootReducer } from "./root.reducer";
 import persistStore from "redux-persist/es/persistStore";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
 const logger = (store) => (next) => (action) => {
     if(!action.type) {
@@ -20,8 +21,13 @@ const logger = (store) => (next) => (action) => {
     console.log('curState: ', store.getState());
 }
 
-const middlewares = [logger, thunk];
+/* const middlewares = [logger, thunk];
 const composedEnchancers = composeWithDevTools(applyMiddleware(...middlewares));
 
-export const store = createStore(rootReducer, undefined, composedEnchancers);
+export const store = createStore(rootReducer, undefined, composedEnchancers); */
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    devTools: process.env.NODE_ENV !== 'production'
+});
 export const persistor = persistStore(store);
