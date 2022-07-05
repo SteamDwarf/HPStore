152679eb818a86bcd41912f1649d2f92eebcadbf
Dmitriy <schajckodmitrij@gmail.com>
2022-07-05 20:50:08 +0300

    some fixes

--- src/components/dropdowns/links-dropdown/links-dropdown.component.jsx
+++ src/components/dropdowns/links-dropdown/links-dropdown.component.jsx
@@ -4,24 +4,30 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getUser } from "../../../redux/user/user.selectors";
 import { setUser } from "../../../redux/user/user.slice";
+import { toggleBurgerMenu } from "../../../redux/themes/themes.slice";
 
 const LinksDropdown = () => {
     const dispatch = useDispatch();
     const user = useSelector(getUser);
+
     const signOut = () => {
         dispatch(setUser(null));
+    }
+    const clickHandler = (additionalFunc) => {
+        additionalFunc();
+        dispatch(toggleBurgerMenu(false));
     }
 
     return (
         <Dropdown>
             <div className='header-links_dropdown-container'>
-                <Link className='header-links_dropdown-link' to='/categories'>ТОВАРЫ</Link>
+                <Link className='header-links_dropdown-link' to='/categories' onClick={clickHandler}>ТОВАРЫ</Link>
                 {
                     user
-                    ?<span className='header-links_dropdown-link' onClick={signOut}>ВЫЙТИ</span>
-                    :<Link className='header-links_dropdown-link' to='/authentification'>ВОЙТИ</Link>
+                    ?<Link className='header-links_dropdown-link' to='#' onClick={() => clickHandler(signOut)}>ВЫЙТИ</Link>
+                    :<Link className='header-links_dropdown-link' to='/authentification' onClick={clickHandler}>ВОЙТИ</Link>
                 }
-                <Link className='header-links_dropdown-link' to='/purchases'>КОРЗИНА</Link>
+                <Link className='header-links_dropdown-link' to='/purchases' onClick={clickHandler}>КОРЗИНА</Link>
             </div>
         </Dropdown>
     );
