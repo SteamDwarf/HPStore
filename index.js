c536a953a316881c1eb4684272cc0108f773bdd0
Dmitriy <schajckodmitrij@gmail.com>
2022-07-08 20:27:25 +0300

    Updates

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
         <Provider store={store}>
           <PersistGate persistor={persistor}>
             <App />
